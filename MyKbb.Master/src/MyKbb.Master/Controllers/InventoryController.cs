13ec6e66fc36d88b08065ad2023e7774666400d3
Prasad Muley <prasad.muley@veriskhealth.com>
2016-03-26 12:34:00 -0600

    PM Wired up dummy to view, added display template for car model

--- MyKbb.Master/src/MyKbb.Master/Controllers/InventoryController.cs
+++ MyKbb.Master/src/MyKbb.Master/Controllers/InventoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using MyKbb.Master.DataAccess;
 using MyKbb.Master.Services;
+using MyKbb.Master.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,7 +22,9 @@ namespace MyKbb.Master.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new InventoryViewModel();
+            viewModel.Cars = _inventoryService.GetCars();
+            return View(viewModel);
         }
     }
 }
--- MyKbb.Master/src/MyKbb.Master/Services/InventoryService.cs
+++ MyKbb.Master/src/MyKbb.Master/Services/InventoryService.cs
@@ -12,19 +12,29 @@ namespace MyKbb.Master.Services
         IList<Car> GetCars(string manufacuters = "", string years = "");
     }
 
-    public class InventoryService: IInventoryService
+    public class InventoryService : IInventoryService
     {
         private KbbContext _kbbContext;
 
         public InventoryService(KbbContext kbbContext)
         {
             _kbbContext = kbbContext;
+            BuildDummyData();
         }
 
         public IList<Car> GetCars(string manufacuters = "", string years = "")
         {
 
-            return null;
+            return _kbbContext.Cars.ToList();
+        }
+
+        private void BuildDummyData()
+        {
+            _kbbContext.Cars.Add(new Car { Id = 1, Manufacturer = "GM", Year = 2000, Model = "Model1", Body = BodyStyle.Coupe, Engine = EngineType.V6, ExteriorColor = "Red", Transmission = TransmissionStyle.Automatic, Price = "1000", SpecialProperty = "", ThumbNail = "" });
+            _kbbContext.Cars.Add(new Car { Id = 2, Manufacturer = "GM", Year = 2000, Model = "Model1", Body = BodyStyle.Coupe, Engine = EngineType.V6, ExteriorColor = "Red", Transmission = TransmissionStyle.Automatic, Price = "1000", SpecialProperty = "", ThumbNail = "" });
+            _kbbContext.Cars.Add(new Car { Id = 3, Manufacturer = "GM", Year = 2000, Model = "Model1", Body = BodyStyle.Coupe, Engine = EngineType.V6, ExteriorColor = "Red", Transmission = TransmissionStyle.Automatic, Price = "1000", SpecialProperty = "", ThumbNail = "" });
+            _kbbContext.Cars.Add(new Car { Id = 4, Manufacturer = "GM", Year = 2000, Model = "Model1", Body = BodyStyle.Coupe, Engine = EngineType.V6, ExteriorColor = "Red", Transmission = TransmissionStyle.Automatic, Price = "1000", SpecialProperty = "", ThumbNail = "" });
+            _kbbContext.SaveChanges();
         }
     }
 }
